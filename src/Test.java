058b575e5770007fc583a8bf73c78dc52333dfd9
cakarakif <cakarakif@users.noreply.github.com>
2018-11-03 17:23:44 +0300

    Completed

---
+++ src/Test.java
@@ -0,0 +1,21 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		RedBlackBST<String, Integer> rbt = new RedBlackBST<String, Integer>();
+		rbt.PutVal("A", 8);
+		rbt.PutVal("C", 4);
+		rbt.PutVal("X", 7);
+		rbt.PutVal("E", 12);
+		rbt.PutVal("H", 5);
+		rbt.PutVal("R", 3);
+		rbt.PutVal("L", 11);
+		rbt.PutVal("M", 9);
+		rbt.PutVal("P", 10);
+		rbt.PutVal("S", 0);
+
+		
+
+	}
+
+}
